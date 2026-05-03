d9f45a719c6de766bbb7bb58788d21e9d966de44
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-05-03 19:32:54 +0300

    force error

--- library-backend/index.js
+++ library-backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const connectToDatabase = require('./db')
 const startServer = require('./server')
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = prdocess.env.MONGODB_URI
 const PORT = process.env.PORT || 4000
 
 const main = async () => {
